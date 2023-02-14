5033e4c2bca750467e21cfb92d8d194138c5b3d0
mateocampillo <mateocampillo1@gmail.com>
2023-02-14 11:41:13 -0300

    mobile#3

---
+++ public/js/botonSidebarMobile.js
@@ -0,0 +1,26 @@
+const funcionMenuSidebarMobile = () => {
+    let botonSidebar = document.querySelector('#botonSidebar');
+    let menuSidebar = document.querySelector('#MenuSidebarWrapper');
+    let body = document.querySelector('body');
+
+    botonSidebar.addEventListener('touchstart', function(e) {
+        e.preventDefault();
+        if(menuSidebar.classList.contains('menuSidebarClose')){
+            menuSidebar.classList.remove('menuSidebarClose');
+            menuSidebar.classList.add('menuSidebarAnimOpen');
+            body.classList.add('stop-scroll');
+        } else if(menuSidebar.classList.contains('menuSidebarAnimClose')){
+            menuSidebar.classList.remove('menuSidebarAnimClose');
+            menuSidebar.classList.add('menuSidebarAnimOpen');
+            body.classList.add('stop-scroll');
+        } else {
+            menuSidebar.classList.remove('menuSidebarAnimOpen');
+            menuSidebar.classList.add('menuSidebarAnimClose');
+            body.classList.remove('stop-scroll');
+        }
+    });
+
+}
+
+
+window.addEventListener('load', funcionMenuSidebarMobile);
