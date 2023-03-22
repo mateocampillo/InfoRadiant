6aac5f9f5da4d1da1f1933168c11ab5c3eca5bbc
mateocampillo <mateocampillo1@gmail.com>
2023-03-22 11:23:53 -0300

    close sidebar when using an anchor

--- src/Components/Sidebar/MenuSidebar.js
+++ src/Components/Sidebar/MenuSidebar.js
@@ -3,22 +3,25 @@ import {
     MenuSidebarContainer
 } from './Sidebar.elements';
 import { Sidebar } from 'react-pro-sidebar';
+import { useProSidebar } from 'react-pro-sidebar';
 
 function MenuSidebar(){
 
+    const { collapseSidebar } = useProSidebar();
+
     return (
-        <Sidebar id='proSidebar' defaultCollapsed={true} collapsedWidth={0} transitionDuration={500}>
+        <Sidebar id='proSidebar' defaultCollapsed={true} collapsedWidth={0} transitionDuration={500} collapsed={true}>
             <MenuSidebarContainer>
-                <div><a href='/agentes'>Agentes</a></div>
-                <div><a href='/rangos'>Rangos</a></div>
-                <div><a href='/armas'>Armas</a></div>
-                <div><a href='/mapas'>Mapas</a></div>
-                <div><a href='/cartas'>Cartas de jugador</a></div>
-                <div><a href='/bundles'>Bundles</a></div>
-                <div><a href='/sprays'>Sprays</a></div>
-                <div><a href='/buddies'>Buddies</a></div>
-                <div><a href='/player-titles'>Titulos de jugador</a></div>
-                <div><a href='/about'>About</a></div>
+                <div><a href='/agentes' onClick={() => collapseSidebar()}>Agentes</a></div>
+                <div><a href='/rangos' onClick={() => collapseSidebar()}>Rangos</a></div>
+                <div><a href='/armas' onClick={() => collapseSidebar()}>Armas</a></div>
+                <div><a href='/mapas' onClick={() => collapseSidebar()}>Mapas</a></div>
+                <div><a href='/cartas' onClick={() => collapseSidebar()}>Cartas de jugador</a></div>
+                <div><a href='/bundles' onClick={() => collapseSidebar()}>Bundles</a></div>
+                <div><a href='/sprays' onClick={() => collapseSidebar()}>Sprays</a></div>
+                <div><a href='/buddies' onClick={() => collapseSidebar()}>Buddies</a></div>
+                <div><a href='/player-titles' onClick={() => collapseSidebar()}>Titulos de jugador</a></div>
+                <div><a href='/about' onClick={() => collapseSidebar()}>About</a></div>
             </MenuSidebarContainer>
         </Sidebar>
     )
